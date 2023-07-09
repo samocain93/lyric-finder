11373ae88b6b23813ad31233655e35e080c6b950
Sam O'Cain <samocain93@gmail.com>
2023-07-09 13:17:17 -0500

    setting up basic components, moving to context

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Navbar from './components/layout/Navbar';
+import Index from './components/layout/Index';
 
 function App() {
   return (
-    <div className="App">
-     <h1>Lyric finder</h1>
+    <div className='app'>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Index />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/components/layout/Index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Tracks from '../tracks/Tracks';
+
+export default function Index() {
+  return (
+    <div>
+      <>
+        <Tracks />
+      </>
+    </div>
+  );
+}
